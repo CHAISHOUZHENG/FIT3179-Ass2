c39f24f4b107cb6dcb5a5d3691c1dfe59c4942f2
CHAISHOUZHENG <ccha0220@student.monash.edu>
2025-10-06 16:20:24 +0800

    Update the latest visualization for homework w10

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,5 +1,8 @@
-var spec = "malaysia_income_map.vg.json";
+// Load Malaysia Choropleth Map
+var mapSpec = "malaysia_income_map.vg.json";
+vegaEmbed('#vis_map', mapSpec, {actions: false}).catch(console.error);
 
-vegaEmbed('#vis', spec).then(function(result) {
-  // You can interact with Vega view instance here
-}).catch(console.error);
+// Load Median Income Area Chart
+var areaSpec = "area_chart.json";
+vegaEmbed('#vis_area', areaSpec, {actions: false}).catch(console.error);
+
